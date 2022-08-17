2a43ed51da4b236425389217d9f85c85af06d6b3
ercanaltun <110672975+ercanaltun@users.noreply.github.com>
2022-08-17 12:20:18 +0300

    Update A01_Slashing.java

--- src/A01_Slashing.java
+++ src/A01_Slashing.java
@@ -6,6 +6,8 @@ public class A01_Slashing {
         System.out.println("Day_3, 3. Trying For Slashing in Ercan");
         System.out.println("Day_3, 4. Trying For Slashing in Ercan");
         System.out.println("Day_3, 5. Trying For Slashing in Ercan");
+        System.out.println("Day_3, 6. Trying For Slashing in GitHub");
+        
 
 
     }
