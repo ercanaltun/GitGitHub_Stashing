5cdaabf40c303a3bcf64e0c855cf04bc4fc91473
Ercan ALTUN <salsel0147@gmail.com>
2022-08-17 11:58:35 +0300

    2.

--- src/A01_Slashing.java
+++ src/A01_Slashing.java
@@ -2,5 +2,7 @@ public class A01_Slashing {
     public static void main(String[] args) {
 
         System.out.println("Day_3, 1. Trying For Slashing");
+        System.out.println("Day_3, 2. Trying For Slashing in Master");
+
     }
 }
